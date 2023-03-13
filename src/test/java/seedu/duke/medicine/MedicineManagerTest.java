4a78e0f896424c4176770badab844754edcf5b4f
JeraldChen <jeraldchenjunhan@gmail.com>
2023-03-13 20:22:00 +0800

    Remove unused import statement

--- src/test/java/seedu/duke/medicine/MedicineManagerTest.java
+++ src/test/java/seedu/duke/medicine/MedicineManagerTest.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 
-
+ 
 class MedicineManagerTest {
     @Test
     public void getDosage_basedOnMedicine_expectCorrectDosage() {
